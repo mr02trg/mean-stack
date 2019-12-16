c95bed91f29266c98be687070d80922129dcce3a
Tue <manhtuetruong@gmail.com>
2019-12-17 07:56:11 +1100

    - implement https for node-server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
-const http = require('http');
+// const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
 const express_app = require('./server');
 require('dotenv').config()
 
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'localhost.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'localhost.crt'))
+};
+
 const port = process.env.PORT || 3000;
-const server = http.createServer(express_app);
+const server = https.createServer(options, express_app);
 
 // start the server
 server.listen(port);
